3183782973dabb0dc530228109d1f0ec651c1d4d
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-27 09:56:34 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Veda Niav Cunniffe on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
